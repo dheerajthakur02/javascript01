4f511c4249679879d24e0d267dc30844b95fb49d
dheerajthakur02 <114847997+dheerajthakur02@users.noreply.github.com>
2024-06-30 09:00:37 +0000

    updated

---
+++ 01basics/01_variables.js
@@ -0,0 +1,5 @@
+const account_id=342566 // once the variable avlue is assigned const cannot be changed further
+let account_email="dk666@google.com"//once the variable avlue is assigned in let can be changed further  it is {} scoped
+var acc_pwd=456123// not use in modern javascript 
+let accountState;
+console.table([account_id,account_email,acc_pwd,accountState])
